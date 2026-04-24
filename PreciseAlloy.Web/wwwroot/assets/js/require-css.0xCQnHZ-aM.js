ef1e757c941478626628b55691a2fdcaa33cfdcf
Build Service <build-service@eshn.dev>
2026-04-24 09:03:12 +0000

    chore(assets): create frontend release package

---
+++ PreciseAlloy.Web/wwwroot/assets/js/require-css.0xCQnHZ-aM.js
@@ -0,0 +1,2 @@
+import{j as r}from"./react-loader.0xCu5eGJYy.js";const o=({path:s,rel:e})=>{const t=/^https?:\/\//gi.test(s)?s:s.startsWith("vendors/")?viteAbsoluteUrl("/assets/"+s+".css"):viteAbsoluteUrl("/assets/css/"+s+".css");return r.jsx("link",{"data-pl-require":!0,href:t,rel:e??"stylesheet"})};export{o as R};
+//# sourceMappingURL=require-css.0xCQnHZ-aM.js.map
--- PreciseAlloy.Web/wwwroot/assets/js/require-css.0xDkRnFA4y.js
+++
@@ -1,2 +0,0 @@
-import{j as r}from"./react-loader.0xBnCiqXXy.js";const o=({path:s,rel:e})=>{const t=/^https?:\/\//gi.test(s)?s:s.startsWith("vendors/")?viteAbsoluteUrl("/assets/"+s+".css"):viteAbsoluteUrl("/assets/css/"+s+".css");return r.jsx("link",{"data-pl-require":!0,href:t,rel:e??"stylesheet"})};export{o as R};
-//# sourceMappingURL=require-css.0xDkRnFA4y.js.map
